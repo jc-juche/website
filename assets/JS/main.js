c6de3a0ca2f8cd1ac07d838d50789b29a42bd4df
Linscon <49949859+Linscon@users.noreply.github.com>
2019-07-25 20:55:27 +0800

    GitTalk
    希望能行
    此外修复了QAQ标题

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,11 +1,12 @@
 var normal_title = "蒟扯---------------=≡Σ((( つ•̀ω•́)つ"
-var QAQ_title = "'你难道不想扯淡了吗？QAQ';"
+var QAQ_title = "你难道不想扯淡了吗？QAQ---------------"
+
 function init()
 {
-	document.addEventListener('visibilitychange',
+	document.addEventListener("visibilitychange",
 	function()
 	{
-		if (document.visibilityState == 'hidden')
+		if (document.visibilityState == "hidden")
 		{
 			normal_title = document.title;
 			document.title = QAQ_title;
@@ -18,4 +19,15 @@ function init()
 		let now_title = document.title;
 		document.title = now_title.substr(-1) + now_title.substr(0, now_title.length - 1);
 	}, 700)
+	var gitalk = new Gitalk(
+	{
+		clientID: "b14b7403f78f2709379b",
+		clientSecret: "21135d81b29daabe125ce9c518017e93fe448475",
+		repo: "website",
+		owner: "Linscon",
+		admin: ["Linscon", "ForkFG"],
+		id: location.href,
+		distractionFreeMode: false
+	});
+	gitalk.render("gitalk_container");
 }
